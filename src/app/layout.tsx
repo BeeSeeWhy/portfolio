5be11dcdecfaf8f3b84286264ecce8713617d0f9
Brandon Cruz-Youll <42663258+BeeSeeWhy@users.noreply.github.com>
2026-05-18 18:02:21 -0700

    switched to app router and fixed several warninggs

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import "@/styles/globals.css";
+import { Montserrat } from "next/font/google";
+import type { Metadata } from "next";
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-mont",
+});
+
+export const metadata: Metadata = {
+  title: "BeeSeeWhy's Portfolio",
+  description: "Brandon Cruz-Youll's Portfolio.",
+  icons: {
+    icon: "/profilealt.ico",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
+        <NavBar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
